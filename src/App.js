c5f864c024f878174fc90800a8e0141a3f8af8ff
Giulia-Verruck-Tortola <giuliaverruck@gmail.com>
2022-05-08 14:32:05 -0300

    Esboço home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+export const Cabecalho = styled.header `
+width: auto;
+background-color: #8F1EAE;
+color: #E52E26;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+`
+
+export const Corpo = styled.div`
+background-color: #F0420D;
+height: 100vh;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cabecalho>
+          <a>Home</a>
+          <a>Produtos</a>
+          <a>Carrinho</a>
+        </Cabecalho>
+        <Corpo>aaaaaaaaaaaaaaaaaaaaaaaaaa</Corpo>
     </div>
   );
 }
