db071f7a498f0bf174ff8fc772226043fe96d22d
fabidbastos <fabidbastos@gmail.com>
2022-05-12 20:43:31 -0300

    labe-commerce

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Filtro from "./components/filtro/Filtro.jsx";
 import Home from "./components/home/Home";
 import nave1 from "./components/imagens/nave1.png";
 import nave2 from "./components/imagens/nave2.png";
@@ -8,20 +9,46 @@ import nave4 from "./components/imagens/nave4.png";
 import nave5 from "./components/imagens/nave5.png";
 import nave6 from "./components/imagens/nave6.png";
 
+
+const Buscador = styled.input`
+padding: 5px;
+`
+
+
 export const Cabecalho = styled.header`
   width: auto;
-  background-color: #8f1eae;
-  color: #e52e26;
+  background-color: #001E5F;
+  color: white;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  padding:5px;
+  font-size: 10px;
+  font-weight: bolder;
+`;
+
+export const Rodape = styled.footer`
+  width: auto;
+  background-color: #001E5F;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  padding:5px;
+  font-size: 10px;
+  font-weight: bolder;
 `;
 
 export const Titulo = styled.div`
-  background-color: #f0420d;
-  font-size: xx-large;
+  background-color: #EA7C01;
+  color: white;
+  font-size: 50px;
   height: 10vh;
   text-align: center;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  text-shadow: 2px 2px #8f1eae;
 `;
 
 export const Corpo = styled.section`
@@ -29,35 +56,78 @@ export const Corpo = styled.section`
   justify-items: center;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
-  background-color: #f0420d;
+  background-image: linear-gradient(#EA7C01,#DA1F34,#8800C2);
+  justify-content: center;
+  align-items: center;
+  text-align: center;
   img {
-    width: 300px;
-    height: 300px;
+    width: 200px;
+    height: 200px;
     align-items: space-between;
     border-style: solid;
-    border-color: #8f1eae;
+    border-color: #EA7C01;
+    border-radius: 10px;
   }
 `;
 
-function App() {
-  return (
-    <div>
-      <Cabecalho>
-        <a>Home</a>
-        <a>Produtos</a>
-        <a>Carrinho</a>
-      </Cabecalho>
-      <Titulo>Space Jamships</Titulo>
-      <Corpo>
-        <Home imagem={nave1} nome="Nave 1" valor="R$3000" />
-        <Home imagem={nave2} nome="Nave 2" valor="R$2500" />
-        <Home imagem={nave3} nome="Nave 3" valor="R$6000" />
-        <Home imagem={nave4} nome="Nave 4" valor="R$8000" />
-        <Home imagem={nave5} nome="Nave 5" valor="R$1500" />
-        <Home imagem={nave6} nome="Nave 6" valor="R$2000" />
-      </Corpo>
-    </div>
-  );
+const produtos = [
+  {id:1,nome:'Nave Revell U.S.S Voyager',valor:'R$: 3.000,00',img:nave1},
+  {id:2,nome:'Nave Tie Figther',valor:'R$: 2.500,00',img:nave2},
+  {id:3,nome:'Nave Millennium Falcon',valor:'R$: 6.000,00',img:nave3},
+  {id:4,nome:'Nave Marciana',valor:'R$: 8.000,00',img:nave4},
+  {id:5,nome:'Onibus Espacial Tesla',valor:'R$: 1.500,00',img:nave5},
+  {id:6,nome:'Nave 6',valor:'R$: 2.000,00',img:nave6},
+];
+
+
+export class App extends React.Component {
+
+  state = {
+    minPreco: "",
+    maxPreco: "",
+  }
+
+  updateMinPreco = (ev) => {
+    this.setState({
+       minPreco: ev.target.value
+    })
+ }
+
+ updateMaxPreco = (ev) => {
+    this.setState({
+       maxPreco: ev.target.value
+    })
+ }
+
+    render(){
+    
+         return (
+        
+        <div>
+          <Cabecalho>
+            <a href="app.js">HOME</a>
+            <a href=" ">PRODUTOS</a>
+            <a href="carrinho.js">CARRINHO</a>
+          </Cabecalho>
+        
+          <Titulo>SPACE JAMSHIPS</Titulo>
+        
+         
+          <Filtro>
+            
+          </Filtro>
+           
+      
+          <Corpo>
+            {produtos.map((item)=>{
+              return <Home key={item.id} imagem={item.img} nome={item.nome}   valor={item.valor} />
+            })}
+          </Corpo>
+        
+          <Rodape>&copy; LOJASPACEJAM </Rodape>
+        </div>
+      
+      )};
 }
 
 export default App;
