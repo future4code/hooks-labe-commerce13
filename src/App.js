f5267b1628b84f5481e6fe51cdab60ac9061fe18
Giulia-Verruck-Tortola <giuliaverruck@gmail.com>
2022-05-13 21:55:57 -0300

    Ajuste Filtro/Busca

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Filtro from "./components/filtro/Filtro.jsx";
+import Filtro from "./components/filtro/Filtro.js";
 import Home from "./components/home/Home";
 import nave1 from "./components/imagens/nave1.png";
 import nave2 from "./components/imagens/nave2.png";
@@ -8,10 +8,16 @@ import nave3 from "./components/imagens/nave3.png";
 import nave4 from "./components/imagens/nave4.png";
 import nave5 from "./components/imagens/nave5.png";
 import nave6 from "./components/imagens/nave6.png";
+import nave7 from "./components/imagens/nave7.jpg";
+import nave8 from "./components/imagens/nave8.jpg";
+import nave9 from "./components/imagens/nave9.jpeg";
 
 
-const Buscador = styled.input`
-padding: 5px;
+
+
+const Buscador = styled.div`
+ background-color: #EA7C01;
+  color: white;
 `
 
 
@@ -71,12 +77,16 @@ export const Corpo = styled.section`
 `;
 
 const produtos = [
-  {id:1,nome:'Nave Revell U.S.S Voyager',valor:'R$: 3.000,00',img:nave1},
-  {id:2,nome:'Nave Tie Figther',valor:'R$: 2.500,00',img:nave2},
-  {id:3,nome:'Nave Millennium Falcon',valor:'R$: 6.000,00',img:nave3},
-  {id:4,nome:'Nave Marciana',valor:'R$: 8.000,00',img:nave4},
-  {id:5,nome:'Onibus Espacial Tesla',valor:'R$: 1.500,00',img:nave5},
-  {id:6,nome:'Nave 6',valor:'R$: 2.000,00',img:nave6},
+  {id:1,nome:'Nave Revell U.S.S Voyager',valor:3000,img:nave1},
+  {id:2,nome:'Nave Tie Figther',valor:2500,img:nave2},
+  {id:3,nome:'Nave Millennium Falcon',valor:6000,img:nave3},
+  {id:4,nome:'Nave Marciana',valor:8000,img:nave4},
+  {id:5,nome:'Ônibus Espacial Tesla',valor:1500,img:nave5},
+  {id:6,nome:'Ônibus Espacial SpaceX S11',valor:2000,img:nave6},
+  {id:7,nome:'Nave Vegana',valor:1000,img:nave8},
+  {id:8,nome:'Disco Voador MIB',valor:2000,img:nave7},
+  {id:9,nome:'Nave Rick and Morty',valor:10000,img:nave9},
+
 ];
 
 
@@ -85,18 +95,36 @@ export class App extends React.Component {
   state = {
     minPreco: "",
     maxPreco: "",
+    query: "",
+    ordem: 1,
+    parametroOrdenacao: "",
+    //produto: produtos
   }
+  atualizarQuery = (ev) => {
+    this.setState({
+       query: ev.target.value
+    })
+ }
 
-  updateMinPreco = (ev) => {
+ filtroMinPreco = (ev) => {
     this.setState({
        minPreco: ev.target.value
     })
  }
 
- updateMaxPreco = (ev) => {
+ filtroMaxPreco = (ev) => {
     this.setState({
        maxPreco: ev.target.value
     })
+ }
+ atualizarParametroOrdenacao = (ev) => {
+   this.setState({
+     parametroOrdenacao: ev.target.value
+   })
+ }
+
+ atualizarOrdem = (ev) => {
+   this.setState({ordem: ev.target.value})
  }
 
     render(){
@@ -112,15 +140,48 @@ export class App extends React.Component {
         
           <Titulo>SPACE JAMSHIPS</Titulo>
         
-         
-          <Filtro>
-            
-          </Filtro>
-           
+          <Buscador>
+          <Filtro
+          query={this.state.query}
+          minPreco={this.state.minPreco}
+          maxPreco={this.state.maxPreco}
+          parametroOrdenacao={this.parametroOrdenacao}
+          atualizarQuery={(ev) => this.atualizarQuery(ev)}
+          atualizarOrdem={this.atualizarOrdem}
+          filtroMaxPreco={(ev) => this.filtroMaxPreco(ev)}
+          filtroMinPreco={(ev) => this.filtroMinPreco(ev)}         
+          />
+          </Buscador>
+             
       
           <Corpo>
-            {produtos.map((item)=>{
-              return <Home key={item.id} imagem={item.img} nome={item.nome}   valor={item.valor} />
+            {produtos
+            .filter (item => {
+              return item.nome.toLowerCase(produtos).includes(this.state.query.toLocaleLowerCase())
+            })
+            .filter (item => {
+              return this.state.minPreco === "" || item.valor >= this.state.minPreco
+            })
+            .filter (item => {
+              return this.state.maxPreco === "" || item.valor <= this.state.maxPreco
+            })
+            // .sort((itemAtual, proximoItem) => {
+            //   // switch (this.state.parametroOrdenacao) {
+            //   //   case "Nome":
+            //   //     return this.state.ordem * itemAtual.nome.localeCompare(proximoItem.nome)
+            //   //   case "Valor":
+            //   //     if ("Crescente")
+            //   //     case 'crescente':
+            //   //       return itemAtual.preco - proximoItem.preco
+            //   //     case 'decrescente':
+            //   //       return proximoItem.preco - itemAtual.preco
+            //   //     default:
+            //   //       return true
+            //   }
+            // })
+        
+          .map(item=>{
+              return <Home key={item.id} imagem={item.img} nome={item.nome}  valor={item.valor} />
             })}
           </Corpo>
         
--- src/components/filtro/Filtro.js
+++ src/components/filtro/Filtro.js
@@ -3,32 +3,74 @@ import styled from 'styled-components'
 
 export const ContainerFiltro = styled.div`
 display:flex;
-
+justify-content: center;
+text-align: center;
+input{
+    width: auto;
+    height: auto;
+    border-radius:10px;
+    margin: 2px;
+}
+select{
+    width: auto;
+    height: auto;
+    border-radius:10px;
+    margin: 2px;
+}
+option{
+    width: auto;
+    height: auto;
+    border-radius:10px;
+    margin: 2px;
+}
 `
 
-class Filtro extends React.Component {
+function Filtro (props) {
 
-    render () {
+
         return (
             <ContainerFiltro>
-                <h3>Filtro</h3>
+                <input 
+                    placeholder="Pesquisa"
+                    value={props.query}
+                    onChange={props.atualizarQuery}
+                    />
 
-                <label>Valor mínimo:</label>
                 <input
-                    type={"number"}
-                    value={this.props.minPreco}
-                    onChange={this.props.filtroMinPreco}
+                    type="number"
+                    placeholder="Preço mínimo"
+                    value={props.minPreco}
+                    onChange={props.filtroMinPreco}
                 />
 
-                <label>Valor máximo:</label>
                 <input
-                    type={"number"}
-                    value={this.props.maxPreco}
-                    onChange={this.props.filtroMaxPreco}
+                    type="number"
+                    placeholder="Preço máximo"
+                    value={props.maxPreco}
+                    onChange={props.filtroMaxPreco}
                 />
+                {/* <label for="sort">Ordenar por: </label> */}
+                <select 
+                    name="sort"
+                    value={props.parametroOrdenacao}
+                    onChange={props.atualizarParametroOrdenacao}
+                >
+                    <option  value="">Selecione</option>
+                    <option  value="Nome">Nome</option>
+                    <option  value="Valor">Valor</option>
+                </select>
+
+                <select
+                    name="order"
+                    value={props.ordem}
+                    onChange={props.atualizarOrdem}
+                >
+                    <option>Selecione</option>
+                    <option value={1}>Crescente</option>
+                    <option value={-1}>Decrescente</option>
+                </select>
             </ContainerFiltro>
-    )}
+    )
+        }
+export default Filtro
 
-}
-
-export default Filtro;
